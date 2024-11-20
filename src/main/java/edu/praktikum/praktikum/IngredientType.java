b046f16a7b77da8eef1794958938a6e966b47a81
Вовк Алексей Анатольевич <avovk@samokat.ru>
2024-11-20 20:15:45 +0300

    add praktikum classes

---
+++ src/main/java/edu/praktikum/praktikum/IngredientType.java
@@ -0,0 +1,11 @@
+package edu.praktikum.praktikum;
+
+/**
+ * Перечисление с типами ингредиентов.
+ * SAUCE – соус
+ * FILLING – начинка
+ */
+public enum IngredientType {
+    SAUCE,
+    FILLING
+}
